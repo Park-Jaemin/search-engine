7fe2d8a9caae7605984a720a7649f105d08cca6b
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2025-08-11 20:25:33 +0900

    Init : 백엔드 기초 프로젝트 구성

---
+++ be-search/src/main/java/com/test/besearch/BeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.test.besearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSearchApplication.class, args);
+    }
+
+}
---
+++ be-search/src/test/java/com/test/besearch/BeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.besearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
